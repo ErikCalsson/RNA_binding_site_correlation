93281ab6f1ab396cd98bd911a8af60d280e4f775
Erik Carlßon <ecarlsson@techfak.uni-bielefeld.de>
2021-05-30 16:31:13 +0200

    added TODOs

---
+++ src/data_calculation.py
@@ -0,0 +1,8 @@
+# imports extern
+import pandas as pd
+import numpy as np
+
+# imports intern
+import src.sequence_pre_calculation as pre_calc
+
+# TODO statistical calculation here
---
+++ src/graph_creation.py
@@ -0,0 +1,11 @@
+# imports extern
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# imports intern
+import src.sequence_pre_calculation as pre_calc
+
+# TODO
+# 1. PCR = Principal Components Regression
