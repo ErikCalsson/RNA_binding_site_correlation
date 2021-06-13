c1c4a2636c1b82955f96e50f644330213de8843d
Erik <ecarlsson@techfak.uni-bielefeld.de>
2021-06-13 20:47:14 +0200

    fixed typo in TODO

--- src/graph_creation.py
+++ src/graph_creation.py
@@ -1,11 +1,18 @@
 # imports extern
-import pandas as pd
-import numpy as np
 import matplotlib
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale
+from sklearn import model_selection
+from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 # imports intern
 import sequence_pre_calculation as pre_calc
 
 # TODO
-# 1. PCR = Principal Components Regression
+# 1. PCA = Principal Components Analysis
