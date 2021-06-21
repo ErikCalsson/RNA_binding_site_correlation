1e6736ff66b2d0e47739ad7cea7d6f07dee1dae5
Erik <ecarlsson@techfak.uni-bielefeld.de>
2021-06-21 10:21:24 +0200

    in progress: making PCA from results from sequnce_pre_calculation

--- src/graph_creation.py
+++ src/graph_creation.py
@@ -16,3 +16,24 @@ import sequence_pre_calculation as pre_calc
 
 # TODO
 # 1. PCA = Principal Components Analysis
+print("make PCA")
+
+# projecting n-dimensional matrix to 2D
+pca_SD_values = PCA(n_components=2)
+pca_matrix_values = pca_SD_values.fit_transform(pre_calc.SD_matrix)
+
+PCA_Df = pd.DataFrame(data=pca_matrix_values,
+                      columns=['pca 1', 'pca 2'])
+
+print('pca variation: {}'.format(pca_SD_values.explained_variance_ratio_))
+
+# plot results for visualization
+#plt.figure()
+#plt.figure(figsize=(10, 10))
+#plt.xticks(fontsize=12)
+#plt.yticks(fontsize=14)
+#plt.xlabel('pca comp 1', fontsize=20)
+#plt.ylabel('pca comp 2', fontsize=20)
+#plt.title("PCA of both datasets", fontsize=20)
+
+
