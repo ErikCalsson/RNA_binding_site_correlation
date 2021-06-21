22b774fb70e4efab075b9f733b14b7271b7ccca9
Erik <ecarlsson@techfak.uni-bielefeld.de>
2021-06-21 10:29:04 +0200

    code cleanup

--- src/sequence_pre_calculation.py
+++ src/sequence_pre_calculation.py
@@ -20,10 +20,6 @@ with open("firstSeq.fa.out", "r") as in_file:
 with open("secondSeq.fa.out", "r") as in_file:
     seq_two = in_file.readlines()
 
-
-# concat seq_one and seq_two to seq_three
-#seq_three = seq_one + seq_two
-
 # k-mer Lists calculation
 
 # k-mer size
@@ -39,10 +35,12 @@ def make_kmer_list(seq, size, index):
     n_kmers = len(seq) - size + 1
     for i in range(n_kmers):
         kmer = seq[i:i + size]
+        # for peak call later
+        list_all.append((kmer, index, i))
         # not allowing duplicates
         if kmer not in tmp_kmers:
             tmp_kmers.append(kmer)
-            list_out.append((kmer, index, i))  # offset = i  (kmer, index, i)
+            list_out.append((kmer, index, i))  # offset = i
     return list_out
 
 
@@ -54,20 +52,13 @@ def remove_duplicates(list_in):
 
 
 sub_seq_index = 0
-#list_unique = []  # only unique k-mers for each sub_seq
 list_unique_one = []
 list_unique_two = []
 list_all = []  # for peak call later
-#for sub_seq in range(0, len(seq_three)):
 for sub_seq in range(0, len(seq_one)):
     # get k-mers for each sub_seq
-    #out_list = make_kmer_list(seq_three[sub_seq], ksize, sub_seq_index)
     out_list = make_kmer_list(seq_one[sub_seq], ksize, sub_seq_index)
-    # add to list_all
-    # list_all.extend(out_list)
-    # remove non unique
-    #list_unique.extend(out_list)  # (remove_duplicates(out_list))  # or remove duplicates instead
-    list_unique_one.extend(out_list)  # (remove_duplicates(out_list))  # or remove duplicates instead
+    list_unique_one.extend(out_list)
     sub_seq_index += 1
 
 
@@ -75,15 +66,11 @@ sub_seq_index = 0
 for sub_seq in range(0, len(seq_two)):
     # get k-mers for each sub_seq
     out_list = make_kmer_list(seq_two[sub_seq], ksize, sub_seq_index)
-    # add to list_all
-    list_all.extend(out_list)
-    # remove non unique
-    list_unique_two.extend(out_list)  # (remove_duplicates(out_list))  # or remove duplicates instead
+    list_unique_two.extend(out_list)
     sub_seq_index += 1
 
 
 # sorting list after kmers
-#list_unique.sort(key=lambda a: a[0])
 list_unique_one.sort(key=lambda a: a[0])
 list_unique_two.sort(key=lambda a: a[0])
 
@@ -96,43 +83,24 @@ pointerC = 0
 
 dict_SD = dict()
 while pointerC < len(list_unique_one) and pointerB < len(list_unique_two) and pointerA < len(list_unique_two):
-#while pointerA < len(list_unique) and pointerB < len(list_unique):
     while pointerB < len(list_unique_two) and list_unique_one[pointerC][0] == list_unique_two[pointerB][0]:
-    #while pointerB < len(list_unique_two) and list_unique_one[pointerA][0] == list_unique_two[pointerB][0]:
-    #while pointerB < len(list_unique) and list_unique[pointerA][0] == list_unique[pointerB][0]:
         pointerB += 1
-# for elem_x in range(0, len(list_unique_one)):
-    # for elem_y in range(0, len(list_unique_two)):
-    #for elem_x in range(pointerA, pointerB):
     for elem_y in range(pointerA, pointerB):
-            # only compare sub_seq of similar length. Here shorter at least 80% of longer one in log 2
-            # TODO length similarity as input variable ?
-            #if np.math.log(len(seq_three[list_unique[elem_x][1]])) >= np.math.log(
-            #        len(seq_three[list_unique[elem_y][1]]) * 0.8) \
-            #        or np.math.log(len(seq_three[list_unique[elem_y][1]])) >= np.math.log(
-            #    len(seq_three[list_unique[elem_x][1]]) * 0.8):
-
-            #if np.math.log(len(seq_one[list_unique_one[elem_x][1]])) >= np.math.log(
-            #    len(seq_two[list_unique_two[elem_y][1]]) * 0.8) \
-            #    or np.math.log(len(seq_two[list_unique_two[elem_y][1]])) >= np.math.log(
-            #    len(seq_one[list_unique_one[elem_x][1]]) * 0.8):
-            if np.math.log(len(seq_one[list_unique_one[pointerC][1]])) >= np.math.log(
-                len(seq_two[list_unique_two[elem_y][1]]) * 0.8) \
-                or np.math.log(len(seq_two[list_unique_two[elem_y][1]])) >= np.math.log(
+        # only compare sub_seq of similar length. Here shorter at least 80% of longer one in log 2
+        # TODO length similarity as input variable ?
+        if np.math.log(len(seq_one[list_unique_one[pointerC][1]])) >= np.math.log(
+            len(seq_two[list_unique_two[elem_y][1]]) * 0.8) \
+            or np.math.log(len(seq_two[list_unique_two[elem_y][1]])) >= np.math.log(
                 np.math.log(len(seq_one[list_unique_one[pointerC][1]]))):
-            # if (elem_x[0] == elem_y[0]):
 
-                # key_part_one = str(list_unique_one[elem_x][1])
-                key_part_one = str(list_unique_one[pointerC][1])
-                #key_part_one = str(list_unique[elem_x][1])
-                key_part_two = "-"
-                key_part_three = str(list_unique_two[elem_y][1])
-                #key_part_three = str(list_unique[elem_y][1])
-                dict_key = key_part_one + key_part_two + key_part_three
-                if dict_key in dict_SD.keys():
-                    dict_SD[dict_key] = ((dict_SD.get(dict_key)) + 1)
-                else:
-                    dict_SD.update({dict_key: 1})
+            key_part_one = str(list_unique_one[pointerC][1])
+            key_part_two = "-"
+            key_part_three = str(list_unique_two[elem_y][1])
+            dict_key = key_part_one + key_part_two + key_part_three
+            if dict_key in dict_SD.keys():
+                dict_SD[dict_key] = ((dict_SD.get(dict_key)) + 1)
+            else:
+                dict_SD.update({dict_key: 1})
     pointerA = pointerB
     pointerB = pointerA + 1
     pointerC += 1
@@ -160,7 +128,6 @@ for key in dict_SD:
     if second_dex > maxB:
         maxB = second_dex
     SD = (200 * dict_SD[key]) / ((len(seq_one[first_dex]) - ksize + 1) + (len(seq_two[second_dex]) - ksize + 1))
-    #SD = (200 * dict_SD[key]) / ((len(seq_three[first_dex]) - ksize + 1) + (len(seq_three[second_dex]) - ksize + 1))
     dict_SD[key] = SD
     sumSD += SD
     zal += 1
@@ -197,10 +164,9 @@ print("wrong: ", wrong)
 print("Range ", maxA, " x ", maxA)
 SD_matrix = []
 SD_row = []
-#Initialize the column.
+
 for j in range(0, maxB):
     SD_row.append(0.0)
-#Append the column to each row.
 for i in range(0, maxA):
     SD_matrix.append(SD_row)
 
