570e8612bb678d26bb7c4d3304b78cd2b1126743
Erik <ecarlsson@techfak.uni-bielefeld.de>
2021-10-31 17:15:06 +0100

    added final output message with test results and commen t-Test values

--- src/output_terminal.py
+++ src/output_terminal.py
@@ -1,16 +1,17 @@
 # imports extern
 
 # imports intern
-import sequence_pre_calculation as calc
-import graph_creation as graph
+# import sequence_pre_calculation as calc
+# import graph_creation as graph
 import data_calculation as dat
-
 
 
 # start terminal output
 def use_terminal():
-    #for i in calc.sim_matrix:
-    # for i in calc.count_matrix:
-    #     print('\t'.join(map(str, i)))
-    print("See output Graph x or Files yz for results")
-    # TODO output message with statistic
+    print("See output barGroupPC1.png or barGroupPC2.png for visualisation of final results")
+    # output message with statistic
+    print("PC1: " + dat.result_PC_1[0] + ",p-value: " + dat.result_PC_1[1])
+    print("PC2: " + dat.result_PC_2[0] + ",p-value: " + dat.result_PC_2[1])
+    print("remember: t > T and alpha > p-value")
+    print("T = [ 0.995 -> 2.576, 0.99 -> 2.326, 0.975 -> 1.96, 0.95 -> 1.645]")
+
