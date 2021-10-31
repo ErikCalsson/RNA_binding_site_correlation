e657ff44bc9f028c9923357ff87d78fa049e9d67
Erik <ecarlsson@techfak.uni-bielefeld.de>
2021-10-31 17:18:35 +0100

    fixed final output message

--- src/output_terminal.py
+++ src/output_terminal.py
@@ -10,8 +10,8 @@ import data_calculation as dat
 def use_terminal():
     print("See output barGroupPC1.png or barGroupPC2.png for visualisation of final results")
     # output message with statistic
-    print("PC1: " + dat.result_PC_1[0] + ",p-value: " + dat.result_PC_1[1])
-    print("PC2: " + dat.result_PC_2[0] + ",p-value: " + dat.result_PC_2[1])
+    print("PC1: ", str(dat.result_PC_1[0]) + ",p-value: ", dat.result_PC_1[1])
+    print("PC2: ", dat.result_PC_2[0], ",p-value: ", dat.result_PC_2[1])
     print("remember: t > T and alpha > p-value")
     print("T = [ 0.995 -> 2.576, 0.99 -> 2.326, 0.975 -> 1.96, 0.95 -> 1.645]")
 
